bdeb598800a9e38deb6622993877646da49eba21
Kirito Guy <69622567+KiritoGuy@users.noreply.github.com>
2022-09-25 02:55:53 +0530

    just a begin [ part bullshit ]
    .

---
+++ cogs/tournament.py
@@ -0,0 +1,38 @@
+import os
+import discord
+import psutil
+from discord.ext import commands
+from discord.ext.commands import BucketType, cooldown
+import motor.motor_asyncio
+import nest_asyncio
+import json
+import random
+from config import DATABASE_URL
+nest_asyncio.apply()
+
+database = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_URL)
+tournament = database["TOURNEYKING"]["SERVER"]
+
+class tournament(commands.Cog):
+    """ Commands related to tournament"""
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("tourney Cog Loaded Succesfully")
+
+    async def new_server(self, server_id : int):
+            new__server = {"server_id": server_id, "active": "no", "registeration_channel": 0, "registeration_category": 0, "registration_time": 0, "confimed_role": 0, "slots": 0}
+            await tournament.insert_one(new__server)
+
+
+    async def new_tournament(self, id : int, registeration_channel: int, registeration_category : int, registration_time : 0, confimed_role : int, slots : int):
+        if id is not None:
+            await tournament.update_one({"server_id": server_id}, {"$set": {"registeration_channel": registeration_channel, 
+                                                              "registeration_category" : registeration_category, 
+                                                              "registration_time" : registration_time, 
+                                                              "confimed_role" : confimed_role, 
+                                                              "slots" : slots}
+                                                    }
+                                       )
