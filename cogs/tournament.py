3645043d56231efced0d307bb85a387be46d4de4
Kirito Guy <69622567+KiritoGuy@users.noreply.github.com>
2022-09-25 04:18:17 +0530

    lmao
    dude.

--- cogs/tournament.py
+++ cogs/tournament.py
@@ -15,20 +15,20 @@ tournament = database["TOURNEYKING"]["SERVER"]
 
 class tournament(commands.Cog):
     """ Commands related to tournament"""
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, client: commands.Bot) -> None:
+        self.client = client
 
     @commands.Cog.listener()
     async def on_ready(self):
         print("tourney Cog Loaded Succesfully")
 
     async def new_server(self, server_id : int):
-            new__server = {"server_id": server_id, "active": "no", "registeration_channel": 0, "registeration_category": 0, "registration_time": 0, "confimed_role": 0, "slots": 0}
+            new__server = {"server_id": server_id, "active": "no", "organizer": 0, "registeration_channel": 0, "registeration_category": 0, "registration_time": 0, "confimed_role": 0, "slots": 0}
             await tournament.insert_one(new__server)
 
 
-    async def new_tournament(self, id : int, registeration_channel: int, registeration_category : int, registration_time : 0, confimed_role : int, slots : int):
-        if id is not None:
+    async def new_tournament(self, server_id : int, registeration_channel: int, registeration_category : int, registration_time : 0, confimed_role : int, slots : int):
+        if server_id is not None:
             await tournament.update_one({"server_id": server_id}, {"$set": {"registeration_channel": registeration_channel, 
                                                               "registeration_category" : registeration_category, 
                                                               "registration_time" : registration_time, 
@@ -36,3 +36,12 @@ class tournament(commands.Cog):
                                                               "slots" : slots}
                                                     }
                                        )
+    async def add_organizer(self, server_id : int, organizers : int)
+        if server_id is not None:
+            await tournament.update_one({"server_id": server_id}, {"$set": {"organizer": organizers}})
+
+
+
+    @app_commands.command(name="start")
+    async def my_command(self, interaction: discord.Interaction) -> None:
+        await interaction.response.send_message("Hello from command 1!", ephemeral=True)
